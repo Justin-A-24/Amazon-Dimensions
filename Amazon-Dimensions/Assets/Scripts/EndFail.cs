dd9c4e0904e06754645c0ed5fe6f966fca8c56f4
Justin-A-24 <35875932+Justin-A-24@users.noreply.github.com>
2018-11-16 21:15:41 -0800

    Created conveyor belt and failure script

---
+++ Amazon-Dimensions/Assets/Scripts/EndFail.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndFail : MonoBehaviour {
+
+    public GameObject packagedObject;
+    public Transform endpoint;
+    public int strikes;
+
+    public Text failText;
+
+    void OnTriggerExit(Collider other)
+    {
+        Destroy(other.gameObject);
+        strikes = strikes + 1;
+    }
+
+	// Use this for initialization
+	void Start () {
+        failText.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (strikes == 3)
+        {
+            failText.text = "You fucking suck.";
+        }
+	}
+}
